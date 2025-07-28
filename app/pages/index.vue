cf70ed8fe962eb0d1435c630248af459addbb9cd
bugugugu <1759743261@qq.com>
2025-07-28 17:31:16 +0800

    修改 header 背景图片的 css 代码

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -431,7 +431,7 @@ onMounted(() => {
       position: relative;
       width: 100%;
       height: 200px;
-      background: url("/img/header-bg.webp") no-repeat center center;
+      background: url("/img/header-bg.webp") no-repeat center;
       background-size: cover;
       display: var(--header-display);
 
