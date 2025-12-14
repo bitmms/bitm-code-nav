b58e7f988b009178cc306a59626dfb7266738b39
bitm <1759743261@qq.com>
2025-12-14 14:01:29 +0800

    修改 favicon.ico 引入方法

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,15 @@
 export default defineNuxtConfig({
-  ssr: true,
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
-  css: ['@/assets/css/global.css'],
-  app: {
-    baseURL: '/bitm-code-nav/',
-  },
+    ssr: true,
+    compatibilityDate: '2025-07-15',
+    devtools: {enabled: true},
+    modules: ['@nuxt/eslint'],
+    css: ['@/assets/css/global.css'],
+    app: {
+        baseURL: '/bitm-code-nav/',
+        head: {
+            link: [
+                {rel: 'icon', type: 'image/x-icon', href: 'favicon.ico'},
+            ]
+        }
+    },
 })
